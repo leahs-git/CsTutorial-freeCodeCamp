b5148e58329d9276492b5db1bb5581257e6599cc
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:51:42 -0230

    Fixed typos

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,10 +1,10 @@
 ﻿string characterName = "John";
 int characterAge;
-characterAge = 35
+characterAge = 35;
 
-Console.WriteLine("There once was a man named John");
+Console.WriteLine("There once was a man named " + characterName);
 Console.WriteLine("He was 70 years old");
-Console.WriteLine("He really like the name John");
+Console.WriteLine("He really liked the name John");
 Console.WriteLine("But didn't like being 70");
 
 Console.ReadLine();
