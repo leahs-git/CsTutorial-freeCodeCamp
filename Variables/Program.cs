0ca3c319908f8987fec136a95f204c13fd36fc44
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:53:20 -0230

    Concatenated the strings

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -3,8 +3,8 @@ int characterAge;
 characterAge = 35;
 
 Console.WriteLine("There once was a man named " + characterName);
-Console.WriteLine("He was 70 years old");
-Console.WriteLine("He really liked the name John");
-Console.WriteLine("But didn't like being 70");
+Console.WriteLine("He was " + characterAge + "70 years old");
+Console.WriteLine("He really liked the name " + characterName);
+Console.WriteLine("But didn't like being" + characterAge);
 
 Console.ReadLine();
