9e1a850cbad07e6d7d6d769143eb2677629386fb
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 16:13:27 -0230

    Final note is about unstored information

--- Data Types/Program.cs
+++ Data Types/Program.cs
@@ -5,8 +5,12 @@ char grade = 'A';
 // numbers
 int age = -30;
 // 3 types of decimal numbers: in inceasing order of precision. Decimal is extremely precise, like if working with money
-float, double, decimal
+// float, double, decimal
     double gpa = 3.3;
 
 // true or false values
 bool isMale = false;
+
+// this information is a constant. It is not stored anywhere.
+Console.WriteLine(30);
+Console.WriteLine(false);
