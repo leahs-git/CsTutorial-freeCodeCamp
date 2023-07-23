b43ae2d033fa0e0d750d13b3706ea6bc89162ff8
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:44:13 -0230

    Created a variable for the name John

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("There once was a man named John");
+﻿string characterName = "John";
+
+
+Console.WriteLine("There once was a man named John");
 Console.WriteLine("He was 70 years old");
 Console.WriteLine("He really like the name John");
 Console.WriteLine("But didn't like being 70");
