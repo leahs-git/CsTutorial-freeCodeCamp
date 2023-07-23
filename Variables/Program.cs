98374adb2ae46d63a34cad076972c14142211a78
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:40:36 -0230

    Switched George's name to John

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("There once was a man named George");
+﻿Console.WriteLine("There once was a man named John");
 Console.WriteLine("He was 70 years old");
-Console.WriteLine("He really like the name George");
+Console.WriteLine("He really like the name John");
 Console.WriteLine("But didn't like being 70");
 
 Console.ReadLine();
