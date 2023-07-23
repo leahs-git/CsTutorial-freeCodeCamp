28c3e9ddeb1770bbc3c0b89510b782e8720532b0
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 16:10:32 -0230

    List of core data types per this tutorial

--- Data Types/Program.cs
+++ Data Types/Program.cs
@@ -2,3 +2,11 @@
 string phrase = "Giraffe Academy";
 char grade = 'A';
 
+// numbers
+int age = -30;
+// 3 types of decimal numbers: in inceasing order of precision. Decimal is extremely precise, like if working with money
+float, double, decimal
+    double gpa = 3.3;
+
+// true or false values
+bool isMale = false;
