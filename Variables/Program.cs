ca89ea16def279e79edf10eea4548db33169b4b7
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:59:29 -0230

    Practiced inserting a new name partway through the story

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -4,6 +4,8 @@ characterAge = 29;
 
 Console.WriteLine("There once was a man named " + characterName);
 Console.WriteLine("He was " + characterAge + " years old");
+
+characterName = "Miguel";
 Console.WriteLine("He really liked the name " + characterName);
 Console.WriteLine("But didn't like being " + characterAge);
 
