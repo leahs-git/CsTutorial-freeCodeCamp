ec55f48631945c991ac0a0d28217b881e1b8acb5
Leah Kirk <leah.kirks@gmail.com>
2023-07-23 15:54:47 -0230

    Removed the forgotten 70 so that the age correctly displays as 35, not 3570

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -3,7 +3,7 @@ int characterAge;
 characterAge = 35;
 
 Console.WriteLine("There once was a man named " + characterName);
-Console.WriteLine("He was " + characterAge + "70 years old");
+Console.WriteLine("He was " + characterAge + " years old");
 Console.WriteLine("He really liked the name " + characterName);
 Console.WriteLine("But didn't like being" + characterAge);
 
