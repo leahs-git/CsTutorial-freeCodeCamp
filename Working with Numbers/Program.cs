9d46ec7bcd818b6e16b599ccaee543b46b8056e5
Leah Kirk <leah.kirks@gmail.com>
2023-07-26 17:26:41 -0230

    Making C# do math

--- Working with Numbers/Program.cs
+++ Working with Numbers/Program.cs
@@ -1,3 +1,9 @@
 ﻿//cool things to do with numbers
 Console.WriteLine("For example:" + -73);
 
+Console.WriteLine(5 + 8);
+Console.WriteLine(5 - 8);
+Console.WriteLine(5 / 8);
+Console.WriteLine(5 * 8);
+
+
