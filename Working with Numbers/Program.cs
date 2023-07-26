e4e2d37c6aab6f5bbbee0abff0d2a0d4e11d2562
Leah Kirk <leah.kirks@gmail.com>
2023-07-26 17:41:03 -0230

    Playing with PEDMAS

--- Working with Numbers/Program.cs
+++ Working with Numbers/Program.cs
@@ -8,3 +8,7 @@ Console.WriteLine(5 * 8);
 
 //Modulo aka give me the remainder. Here I expect print result to be 3
 Console.WriteLine(8 % 5);
+
+//How to specify order of operations
+Console.WriteLine(4 + 2 * 3);
+Console.WriteLine((4 + 2) * 3);
