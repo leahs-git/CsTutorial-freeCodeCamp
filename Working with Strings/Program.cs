339628e311876722980434693934f9ecb2d8b66b
Leah Kirk <leah.kirks@gmail.com>
2023-07-26 16:51:58 -0230

    This is how I can keep the quote punctuation in a string

--- Working with Strings/Program.cs
+++ Working with Strings/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// line break
 Console.WriteLine("Good\nDay");
+
+//keep the quotation marks in the print output
+Console.WriteLine("Good\"Day");
 
 
