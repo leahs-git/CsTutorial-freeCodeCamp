67df40b0b46c46d1e54e88b95ca4bc46b5fb971d
Leah Kirk <leah.kirks@gmail.com>
2023-07-26 17:43:13 -0230

    Both of these give the same result (13.1) regardless that one of the numbers is an integer.

--- Working with Numbers/Program.cs
+++ Working with Numbers/Program.cs
@@ -12,3 +12,8 @@ Console.WriteLine(8 % 5);
 //How to specify order of operations
 Console.WriteLine(4 + 2 * 3);
 Console.WriteLine((4 + 2) * 3);
+
+//Difference between using integers and decimal numbers
+
+Console.WriteLine(5.0 + 8.1);
+Console.WriteLine(5 + 8.1);
