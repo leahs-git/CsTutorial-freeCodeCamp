c4f831a5069e51bccc8e1383cd124bf3cfc6ae03
Leah Kirk <leah.kirks@gmail.com>
2023-07-26 16:54:00 -0230

    Store the string as a variable then print the variable

--- Working with Strings/Program.cs
+++ Working with Strings/Program.cs
@@ -4,4 +4,9 @@ Console.WriteLine("Good\nDay");
 //keep the quotation marks in the print output
 Console.WriteLine("Good\"Day");
 
+//store a string as a variable
+
+string phrase = "...Good Day";
+Console.WriteLine(phrase);
+
 
